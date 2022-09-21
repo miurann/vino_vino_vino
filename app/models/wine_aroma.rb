1126eaa1d18b950c7fcd4c17eb16a593a9849760
miurann <mn.camissha@gmail.com>
2022-09-21 23:42:53 +0900

    Add:中間テーブルにユニーク制約追加

--- app/models/wine_aroma.rb
+++ app/models/wine_aroma.rb
@@ -1,4 +1,6 @@
 class WineAroma < ApplicationRecord
   belongs_to :wine
   belongs_to :aroma
+
+  validates :wine_id, uniqueness: { scope: :aroma_id }
 end
--- app/models/wine_grape.rb
+++ app/models/wine_grape.rb
@@ -1,4 +1,6 @@
 class WineGrape < ApplicationRecord
   belongs_to :wine
   belongs_to :grape_variety
+
+  validates :wine_id, uniqueness: { scope: :grape_variety_id }
 end
